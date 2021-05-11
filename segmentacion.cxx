36b7112a3d02080d93f7084d76f7a7ed19b8556c
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2021-05-10 22:42:56 -0500

    mejora en los tipos

--- segmentacion.cxx
+++ segmentacion.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <queue>
 using namespace cv;
@@ -9,21 +10,24 @@ struct point
     int i;
     int j;
 };
-
+typedef std::vector<Point2f> Vector;
 typedef std::queue<point> Queue;
-Mat crecimiento_regiones(Mat image, Queue frente);
-void Crecimiento_Regiones_aux(Mat *image, Mat *reg, int i, int j, bool **est);
+void crecimiento_regiones(Mat image, Queue frente, Mat *etiquetada, Mat *segmentada);
 float desviacion(Mat *image, Queue neighbours);
 Queue getNeighbours(Mat *image, point p);
 void print(Queue c);
 bool aceptacion(Mat *image, int i, int j);
 float promedio(Mat *image, Queue neighbours);
+Queue semillas(Mat image, int cantidad);
+bool similaridad(Mat *image, Queue p1, Queue p2);
+float mayorContraste(Mat *image, Queue neighbours);
 float criterio;
-
+int vecinos;
+int tipo;
 int main(int argc, char const *argv[])
 {
 
-    if (argc < 3)
+    if (argc < 2)
     {
         std::cerr << "Usage: " << argv[0] << std::endl;
         return (-1);
@@ -44,33 +48,75 @@ int main(int argc, char const *argv[])
         std::cerr << "Error: No image data" << std::endl;
         return (-1);
     }
-    criterio = atof(argv[2]);
-    /*MatIterator_<Vec3b> it,end;
-    it = image.begin<Vec3b>();
-    end = image.end<Vec3b>();
+    else
+    {
+        std::cout << "Informacion de la imagen" << std::endl
+                  << "-----Tamaño------\n"
+                  << "Ancho(columnas): " << image.cols << std::endl
+                  << "Alto(filas): " << image.rows << std::endl;
+    }
 
-    for (; it != end; it++)
-    {
-        std::cout<<"R: "<<(unsigned int)(*it)[2]<<"G: "<<(unsigned int)(*it)[1]<<"B: "<<(unsigned int)(*it)[0]<<std::endl;
-    }*/
-    /*for (int i = 0; i < image.rows; i++)
-    {
-        for (int j = 0; j < image.cols; j++)
-        {
-          std::cout<<"R: "<<(unsigned int)(image.at<Vec3b>(i,j))[2]<<"G: "<<(unsigned int)(image.at<Vec3b>(i,j))[1]<<"B: "<<(unsigned int)(image.at<Vec3b>(i,j))[0]<<std::endl;  
-        }
-    }*/
-    //std::cout<<"c "<<image.cols <<" r"<<image.rows<<std::endl;
-    point p;
-    p.i = image.rows / 2;
-    p.j = image.cols / 2;
+    //menu
+    std::cout << "------Menu------" << std::endl
+              << std::endl;
+    std::cout << "indique la fomra de meida de similaridad "
+              << "Desviacion(1)"
+              << " Promedio(2)"
+              << " contraste(3) : ";
+    std::cin >> tipo;
+    std::cout << "inidque criterio de aceptacion(valor flotante): ";
+    std::cin >> criterio;
+    std::cout << "Cantidad de vecinos (4 o 8) :";
+    int vecinos;
+    std::cin >> vecinos;
+    
+    std::cout << "semillas automaticas si(1)/no(0): ";
+    int automaticas;
+    std::cin >> automaticas;
+    int noSemillas;
+    std::cout << "inidque numero de semillas: ";
+    std::cin >> noSemillas;
     Queue cola;
-    cola.push(p);
-    Mat final = crecimiento_regiones(image, cola);
+    if (automaticas == 1)
+        cola = semillas(image, noSemillas);
+    else
+    {
+        for (int i = 0; i < noSemillas; i++)
+        {
+            point p;
+            std::cout << "digite el eje x :";
+            std::cin >> p.j;
+            std::cout << "digite el eje y :";
+            std::cin >> p.i;
+            cola.push(p);
+        }
+    }
+    Mat etiquetada = Mat::zeros(image.rows, image.cols, image.type());
+    Mat segmentada = Mat::zeros(image.rows, image.cols, image.type());
+    crecimiento_regiones(image, cola, &etiquetada, &segmentada);
 
-    imwrite("resultado.png", final);
+    imwrite("etiquetada.png", etiquetada);
+    imwrite("segmentada.png", segmentada);
 }
-Mat crecimiento_regiones(Mat image, Queue frente)
+Queue semillas(Mat image, int cantidad)
+{
+    srand(time(NULL));
+    Queue seads;
+    for (int i = 0; i < cantidad; i++)
+    {
+        point p;
+
+        p.i = rand() % image.rows;
+        p.j = rand() % image.cols;
+        /*
+        p.i = image.rows / 2;
+        p.j = image.cols / 2;*/
+        seads.push(p);
+    }
+    return seads;
+}
+
+void crecimiento_regiones(Mat image, Queue frente, Mat *etiquetada, Mat *segmentada)
 {
     Mat result = Mat::zeros(image.rows, image.cols, image.type());
     Queue cola;
@@ -95,56 +141,81 @@ Mat crecimiento_regiones(Mat image, Queue frente)
     }
     while (!cola.empty())
     {
-        
+
         float des1, des2;
         point p = cola.front();
         cola.pop();
-        result.at<Vec3b>(p.i, p.j) = image.at<Vec3b>(p.i, p.j);
-        //result.at<Vec3b>(p.i, p.j) = {255,255,255};
+        segmentada->at<Vec3b>(p.i, p.j) = image.at<Vec3b>(p.i, p.j); //apllay color
+        etiquetada->at<Vec3b>(p.i, p.j) = {255, 255, 255};
         if (est[p.i][p.j] == false)
         {
             est[p.i][p.j] = true;
-            //std::cout<<"while "<< cola.size()<<std::endl;
         }
         else
         {
             continue;
         }
-        //deviation
 
         Queue c = getNeighbours(&image, p);
-        //print(c);
-        des1 = desviacion(&image, c);
+
+        //des1 = desviacion(&image, c);
 
         //get neighbours
         while (!c.empty())
         {
-            
+
             point neigh = c.front();
             Queue neighbours = getNeighbours(&image, neigh);
-            des2 = desviacion(&image, neighbours);
+            //des2 = desviacion(&image, neighbours);
             //std::cout << abs(des1 - des2) << std::endl;
-            if (abs(des1 - des2) < criterio )
+            if (similaridad(&image, c, neighbours))
             {
-                //std::cout<<"while2"<<std::endl;
                 cola.push(c.front());
             }
             c.pop();
         }
-        //std::cout<<"while2end"<<std::endl;
     }
-    std::cout << "whileend" << std::endl;
-
-    return result;
 }
-void print(Queue c){
+bool similaridad(Mat *image, Queue p1, Queue p2)
+{
+    if (tipo == 1) //desviacion estandar
+        return (abs(desviacion(image, p1) - desviacion(image, p2)) <= criterio);
+    if (tipo == 2)
+        return (abs(promedio(image, p1) - promedio(image, p2)) <= criterio);
+    //std::cout << abs(mayorContraste(image, p1) - mayorContraste(image, p2)) << std::endl;
+    return (abs(mayorContraste(image, p1) - mayorContraste(image, p2)) <= criterio);
+}
+float mayorContraste(Mat *image, Queue neighbours)
+{
+    float mayorR = 0;
+    float mayorG = 0;
+    float mayorB = 0;
+
+    while (!neighbours.empty())
+    {
+        point p = neighbours.front();
+        neighbours.pop();
+        if (image->at<Vec3b>(p.i, p.j)[0] > mayorB || image->at<Vec3b>(p.i, p.j)[1] > mayorG || image->at<Vec3b>(p.i, p.j)[2] > mayorR)
+        {
+            mayorB = image->at<Vec3b>(p.i, p.j)[0];
+            mayorG = image->at<Vec3b>(p.i, p.j)[1];
+            mayorR = image->at<Vec3b>(p.i, p.j)[2];
+        }
+    }
+    if (mayorB >= mayorG && mayorB >= mayorR)
+        return mayorB;
+    if (mayorG > mayorB && mayorG > mayorR)
+        return mayorG;
+    return mayorR;
+}
+void print(Queue c)
+{
     while (!c.empty())
     {
         point p = c.front();
         c.pop();
         std::cout << "i: " << p.i << " j: " << p.j << std::endl;
     }
-    
 }
 bool aceptacion(Mat *image, int i, int j)
 {
@@ -183,35 +254,37 @@ Queue getNeighbours(Mat *image, point p)
     p1.j = p.j;
     if (aceptacion(image, p1.i, p1.j))
         cola.push(p1); //up
+    if (vecinos == 8)
+    {
+        p1.i = p.i - 1;
+        p1.j = p.j + 1;
+        if (aceptacion(image, p1.i, p1.j))
+            cola.push(p1); //up right
 
-    p1.i = p.i - 1;
-    p1.j = p.j + 1;
-    if (aceptacion(image, p1.i, p1.j))
-        cola.push(p1); //up right
+        p1.i = p.i - 1;
+        p1.j = p.j - 1;
+        if (aceptacion(image, p1.i, p1.j))
+            cola.push(p1); //up left
 
-    p1.i = p.i - 1;
-    p1.j = p.j - 1;
-    if (aceptacion(image, p1.i, p1.j))
-        cola.push(p1); //up left
+        p1.i = p.i + 1;
+        p1.j = p.j + 1;
+        if (aceptacion(image, p1.i, p1.j))
+            cola.push(p1); //down right
 
-    p1.i = p.i + 1;
-    p1.j = p.j + 1;
-    if(aceptacion(image, p1.i, p1.j))
-        cola.push(p1); //down right
-
-    p1.i = p.i + 1;
-    p1.j = p.j - 1;
-    if (aceptacion(image, p1.i, p1.j))
-        cola.push(p1); //down left
+        p1.i = p.i + 1;
+        p1.j = p.j - 1;
+        if (aceptacion(image, p1.i, p1.j))
+            cola.push(p1); //down left
+    }
     return cola;
 }
 float promedio(Mat *image, Queue neighbours)
 {
     float tam = neighbours.size();
-    float prom= 0;
+    float prom = 0;
     while (!neighbours.empty())
     {
-       
+
         point p = neighbours.front();
         neighbours.pop();
         for (int i = 0; i < 3; i++)
